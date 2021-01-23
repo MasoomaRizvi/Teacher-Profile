7a9d0ffca4d0af71e31a310791bd8d1b5307cefb
MasoomaRizvi <75495095+MasoomaRizvi@users.noreply.github.com>
2021-01-23 10:26:55 -0800

    TeacherProfile.java

---
+++ TeacherProfile.java
@@ -0,0 +1,15 @@
+TeacherProfile.java
+package com.example.userprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeacherProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_profile);
+    }
+}
